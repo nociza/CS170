4d74782a4eac9322346f0ede4f4f88a972a8f9e9
nociza <56178962+nociza@users.noreply.github.com>
2021-05-03 23:47:16 -0700

    enhanced large

--- solver copy.py
+++ solver copy.py
@@ -8,6 +8,7 @@ import glob
 
 
 
+
 def solve(G):
     """
     Args:
@@ -16,66 +17,58 @@ def solve(G):
         c: list of cities to remove
         k: list of edges to remove
     """
-    return simulatedAnnealing(0.7, G)
+    return simulatedAnnealing(0.5, G)
 
 
 def simulatedAnnealing(initialTreshold, G):
     G = G.copy()
     endNode = len(G.nodes) - 1
     deletedEdges, deletedNodes = [], []
+
+    def naiveDistribution(length):
+        return [1/length for i in range(length)]
+
     def nodeRemover(threshold, nodesRemoved):
         if nodesRemoved >= MAX_NODES_REMOVED or threshold >= 1:
             return
-        largestYet, diff = None, 0 
         allnodes = nx.dijkstra_path(G, 0, endNode)
-        random.shuffle(allnodes)
+        improvementScore = []
+        noPossibleUpdates = True
         for i in allnodes:
             if i == 0 or i == endNode:
+                improvementScore.append(0)
                 continue
-            currentDiff = node_diff(G, i, endNode)
-            if currentDiff and random.random() > threshold:
-                if random.random() > 0.5 and deletedNodes:
-                    deletedNodes.remove(random.choice(deletedNodes))
-                    return nodeRemover(threshold, nodesRemoved - 1)
-                else:
-                    largestYet = i
-                break
-            elif currentDiff and currentDiff > diff:
-                largestYet = i
-                diff = currentDiff
-        if largestYet:
-            deletedNodes.append(largestYet)
-            G.remove_node(largestYet)
-            return nodeRemover(threshold + 0.001, nodesRemoved + 1)
+            score = node_diff(G, i, endNode)
+            if score:
+                noPossibleUpdates = False
+            improvementScore.append(score if score else 0)
+        if noPossibleUpdates:
+            return 
+        chosenNode = random.choices(allnodes, weights=improvementScore, k = 1)
+        deletedNodes.append(chosenNode[0])
+        G.remove_node(chosenNode[0])
+        return nodeRemover(threshold + 0.001, nodesRemoved + 1)
     
     def edgeRemover(threshold, edgesRemoved):
         if edgesRemoved >= MAX_EDGES_REMOVED or threshold >= 1:
             return
-        largestYet, diff = None, 0 
         previous = 0
-        edges = []
+        edges, distribution, improvementScore = [], [], []
+        noPossibleUpdates = True
         for i in nx.dijkstra_path(G, 0, endNode)[1:]:              
             edges.append((previous, i))
+            #distribution.append(G[previous][i]["weight"])
+            score = edge_diff(G, (previous, i), endNode)
+            if score:
+                noPossibleUpdates = False
+            improvementScore.append(score if score else 0)
             previous = i 
-
-        random.shuffle(edges)
-        for i in edges:
-            currentDiff = edge_diff(G, i, endNode)               
-            if currentDiff and random.random() > threshold:
-                if random.random() > 0.5 and deletedEdges:
-                    deletedEdges.remove(random.choice(deletedEdges))
-                    return edgeRemover(threshold, edgesRemoved - 1)
-                else:
-                    largestYet = i
-                break
-            elif currentDiff and currentDiff > diff:
-                largestYet = i
-                diff = currentDiff
-
-        if largestYet:
-            deletedEdges.append(largestYet)
-            G.remove_edge(largestYet[0],largestYet[1])
-            return edgeRemover(threshold + 0.001, edgesRemoved + 1)
+        if noPossibleUpdates:
+            return 
+        chosenEdge = random.choices(edges, weights=improvementScore, k = 1)
+        deletedEdges.append(chosenEdge[0])
+        G.remove_edge(chosenEdge[0][0],chosenEdge[0][1])
+        return edgeRemover(threshold + 0.001, edgesRemoved + 1)
 
     
     nodeRemover(initialTreshold, 0)
@@ -108,11 +101,11 @@ if __name__ == '__main__':
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 if __name__ == '__main__':
-    for i in range(1):
-        inputs = glob.glob('inputs/large/*')
+    for i in range(5):
+        inputs = glob.glob('inputs/medium/*')
         count = 1
         for input_path in inputs:
-            output_path = 'outputs/large/' + basename(normpath(input_path))[:-3] + '.out'
+            output_path = 'outputs/medium/' + basename(normpath(input_path))[:-3] + '.out'
             G = read_input_file(input_path)
             num_nodes = G.number_of_nodes()
             num_edges = G.number_of_edges()
@@ -126,7 +119,7 @@ if __name__ == '__main__':
                 MAX_EDGES_REMOVED = 100
                 MAX_NODES_REMOVED = 5
             resultc, resultk, largest = None, None, 0
-            for i in range(1):
+            for i in range(5):
                 c, k = solve(G)
                 if not is_valid_solution(G, c, k):
                     continue
